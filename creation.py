0671f3e168b823cb1cb67d50c9125d67f183e8f5
Louis GOLLIOT <louis.golliot.etu@univ-lille.fr>
2025-08-16 03:29:17 +0200

    fix l'encodage des fichiers python créer

--- creation.py
+++ creation.py
@@ -6,6 +6,7 @@ def create_loop(x):
         path = f".\\{x}\\jour{i}"
         os.mkdir(path)
         with open(path+"\\sol.py","w") as f :
+            f.write('# -*- coding: utf-8 -*- \n')
             f.write('if __name__ == "__main__" : \n')
             f.write('   print("enigme pas encore traiter") \n')
         with open(path+"\\input.txt","w") as f :
@@ -15,14 +16,19 @@ def create_loop(x):
 
 
 if __name__ == "__main__":
-    x=2030
-    
+    x=""
     while x is not int :
         try:
             x = int(input('What year will be created'))
             os.mkdir(str(x))
+            create_loop(x)
             break
         except ValueError:
             print("it's not a year")
+            with open("sol.py","w") as f :
+                f.write('# -*- coding: utf-8 -*- \n')
+                f.write('if __name__ == "__main__" : \n')
+                f.write('   print("enigme pas encore traiter") \n')
+
     
-    create_loop(x)
+    
