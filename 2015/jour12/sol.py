3ac84981a51315204a2b7eaf91dcc0c2e5315b6f
Louis GOLLIOT <louis.golliot.etu@univ-lille.fr>
2025-08-17 16:00:15 +0200

    jour 12

--- 2015/jour12/sol.py
+++ 2015/jour12/sol.py
@@ -1,3 +1,35 @@
 # -*- coding: utf-8 -*- 
+import json
+
+
+def add_json(data):
+   if isinstance(data,str) or isinstance(data,bool):
+      return 0 
+   elif isinstance(data,int):
+      return data
+   else:
+      if isinstance(data,list):
+         res=0
+         for i in data :
+            res+=add_json(i)
+         return res
+      else:
+         res=0
+         if "red" in data or "red" in data.values():
+            return 0
+         for k,v in data.items():
+            res += add_json(v)
+         return res
+      
+
+
 if __name__ == "__main__" : 
-   print("enigme pas encore traiter") 
+   print("jour 12 d'advent of code 2015") 
+
+   with open('2015/jour12/input.json','r') as f :
+      data = json.load(f)
+   res = add_json(data)
+
+   print(f"la somme de tout les nombres dans le documents est {res}")
+
+
