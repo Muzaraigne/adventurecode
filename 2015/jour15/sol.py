1a25d26040545a6cf836c0e74e3669a43741fee0
Louis GOLLIOT <louis.golliot.etu@univ-lille.fr>
2025-08-23 18:18:50 +0200

    jour 15 et 16

--- 2015/jour15/sol.py
+++ 2015/jour15/sol.py
@@ -1,3 +1,48 @@
 # -*- coding: utf-8 -*- 
+import json
+
+
+def calculs_part1(data) :
+   meuilleure_combinaison = 0
+   for i in range(101) :
+      for j in range(101-i) :
+         for k in range(101-i-j) :
+            x = 100 - i - j - k
+            score = 1
+            for prop in ['capacity','durability','flavor','texture'] :
+               somme = i*data["Sugar"][prop] + j*data["Sprinkles"][prop] + k*data["Candy"][prop] + x*data["Chocolate"][prop]
+               if somme < 0 : 
+                  score = 0
+                  break
+               score *= somme
+            if score > meuilleure_combinaison :
+               meuilleure_combinaison = score
+   return meuilleure_combinaison
+   
+def calculs_part2(data) :
+   meuilleure_combinaison = 0
+   for i in range(101) :
+      for j in range(101-i) :
+         for k in range(101-i-j) :
+            x = 100 - i - j - k
+            calories = i*data["Sugar"]['calories'] + j*data["Sprinkles"]['calories'] + k*data["Candy"]['calories'] + x*data["Chocolate"]['calories']
+            if calories != 500 : 
+               continue
+            score = 1
+            for prop in ['capacity','durability','flavor','texture'] :
+               somme = i*data["Sugar"][prop] + j*data["Sprinkles"][prop] + k*data["Candy"][prop] + x*data["Chocolate"][prop]
+               if somme < 0 : 
+                  score = 0
+                  break
+               score *= somme
+            if score > meuilleure_combinaison :
+               meuilleure_combinaison = score
+   return meuilleure_combinaison
+
 if __name__ == "__main__" : 
-   print("enigme pas encore traiter") 
+   with open('2015/jour15/input.json','r') as f :
+      data = json.load(f)
+   
+   print("--- jour 15 : La science pour les gens affamés (Solution) ---")
+   print("Partie 1 : Le score de la meilleure combinaison est de",calculs_part1(data))
+   print("Partie 2 : Le score de la meilleure combinaison avec 500 calories est de",calculs_part2(data))
--- 2015/jour16/sol.py
+++ 2015/jour16/sol.py
@@ -1,3 +1,62 @@
 # -*- coding: utf-8 -*- 
+def part1(data, critere) : 
+   for (nom,attributs) in data :
+      trouve = True
+      cle = list(attributs.keys())
+      valeur = list(attributs.values())
+      i = 0
+      while i < len(attributs) and trouve :
+         if critere[cle[i]] != valeur[i] : 
+            trouve = False
+         i += 1
+      if trouve : 
+         return nom
+   return None
+
+def part2(data, critere) :
+   for (nom,attributs) in data :
+      trouve = True
+      cle = list(attributs.keys())
+      valeur = list(attributs.values())
+      i = 0
+      while i < len(attributs) and trouve :
+         if cle[i] in ['cats','trees'] : 
+            if critere[cle[i]] >= valeur[i] : 
+               trouve = False
+         elif cle[i] in ['pomeranians','goldfish'] : 
+            if critere[cle[i]] <= valeur[i] : 
+               trouve = False
+         else : 
+            if critere[cle[i]] != valeur[i] : 
+               trouve = False
+         i += 1
+      if trouve : 
+         return nom
+   return None
+
 if __name__ == "__main__" : 
-   print("enigme pas encore traiter") 
+   print("-- Jour 16 : Tanbte Sue (Solution) --")
+
+   with open("2015/jour16/input.txt", "r") as fichier :
+      lignes = fichier.read().splitlines()
+      for i in range(0,len(lignes)): 
+         t = lignes[i].replace(",","").replace(":","").split(" ")
+         lignes[i] = (t[1], {t[2]:int(t[3]), t[4]:int(t[5]), t[6]:int(t[7])})
+
+   critere = {
+      'children': 3,
+      'cats': 7, 
+      'samoyeds': 2,
+      'pomeranians': 3,
+      'akitas': 0,
+      'vizslas': 0,
+      'goldfish': 5,
+      'trees': 3,
+      'cars': 2,
+      'perfumes': 1
+   }
+   
+   print("Partie 1 : ", part1(lignes, critere))
+   print("Partie 2 : ", part2(lignes, critere))
+
+
