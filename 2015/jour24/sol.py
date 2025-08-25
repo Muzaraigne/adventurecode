c4667d10f6f0b5ac9e7a33019be7e5f58c39ecef
Louis GOLLIOT <louis.golliot.etu@univ-lille.fr>
2025-08-25 23:01:37 +0200

    last days

--- 2015/jour24/sol.py
+++ 2015/jour24/sol.py
@@ -1,3 +1,37 @@
-# -*- coding: utf-8 -*- 
-if __name__ == "__main__" : 
-   print("enigme pas encore traiter") 
+import itertools
+import math
+
+def ind_sums(liste, somme) :
+    """ Renvoie les indices des éléments de liste dont la somme vaut somme """
+    n = len(liste)
+    for r in range(1, n+1) :
+      for comb in itertools.combinations(range(n), r) :
+        if sum(liste[i] for i in comb) == somme :
+            yield comb
+
+def resolve(l_colis,repartitions):
+    total = sum(l_colis)
+    cible = total // repartitions
+    solutions_qe = []
+    for r in range(1, len(l_colis) + 1):
+        for indices1 in itertools.combinations(range(len(l_colis)), r):
+            if sum(l_colis[i] for i in indices1) == cible:
+                reste_indices = [i for i in range(len(l_colis)) if i not in indices1]
+                reste = [l_colis[i] for i in reste_indices]
+                groupe2_existe = any(1 for _ in ind_sums(reste, cible))
+                if groupe2_existe:
+                    qe = math.prod(l_colis[i] for i in indices1)
+                    solutions_qe.append(qe)       
+        if solutions_qe:
+            return min(solutions_qe)
+
+    return None
+
+if __name__ == "__main__":
+    print("--- Jour 24 : Soluce ---")
+    with open("2015/jour24/input.txt") as f:
+        lignes = f.read().split("\n")
+    l_colis = [int(li) for li in lignes if li]
+    
+    print("Partie 1 :", resolve(l_colis, 3))
+    print("Partie 2 :", resolve(l_colis, 4))
--- 2015/jour25/sol.py
+++ 2015/jour25/sol.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*- 
+def calculer_code(my_obj) : 
+   row = my_obj["row"] 
+   col = my_obj["col"] 
+   n = row + col - 1 
+   code = 20151125 
+   for i in range(1, n * (n - 1) // 2 + col) : 
+      code = (code * 252533) % 33554393 
+   return code
+
 if __name__ == "__main__" : 
-   print("enigme pas encore traiter") 
+   print("--- Jour 25 : Laissez le neiger (Solution) ---") 
+   my_obj = {"row": 2981, "col": 3075}
+   print("Le code est : ", calculer_code(my_obj))
