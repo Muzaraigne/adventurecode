c628d1adcaefa8b2de1c6063c26e9a2d85422b55
Louis GOLLIOT <louis.golliot.etu@univ-lille.fr>
2025-09-08 00:03:12 +0200

    jour 13 et 14 de 2016

--- 2016/jour13/sol.py
+++ 2016/jour13/sol.py
@@ -1,5 +1,79 @@
 # -*- coding: utf-8 -*- 
+from collections import deque
+
+def bfs_shortest_path(favorite_number, target):
+    start = (1, 1)
+    visited = set()
+    queue = deque()
+    
+    # Chaque élément de la file sera (x, y, steps)
+    queue.append((start[0], start[1], 0))
+    visited.add(start)
+
+    while queue:
+        x, y, steps = queue.popleft()
+
+        if (x, y) == target:
+            return steps
+
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nx, ny = x + dx, y + dy
+
+            # On ignore les coordonnées négatives
+            if nx < 0 or ny < 0:
+                continue
+
+            if is_open(nx, ny, favorite_number) and (nx, ny) not in visited:
+                visited.add((nx, ny))
+                queue.append((nx, ny, steps + 1))
+
+def solve_part2(favorite_number,steps_max):
+    start = (1, 1)
+    visited = set()
+    queue = deque()
+    
+    # Chaque élément de la file sera (x, y, steps)
+    queue.append((start[0], start[1], 0))
+    visited.add(start)
+
+    while queue:
+        x, y, steps = queue.popleft()
+
+        if steps > steps_max:
+            continue
+
+        for dx, dy in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
+            nx, ny = x + dx, y + dy
+
+            # On ignore les coordonnées négatives
+            if nx < 0 or ny < 0:
+                continue
+
+            if is_open(nx, ny, favorite_number) and (nx, ny) not in visited:
+                if steps + 1 <= steps_max:
+                    visited.add((nx, ny))
+                    queue.append((nx, ny, steps + 1))
+
+
+    return len(visited)
+
+
+def is_open(x, y, favorite_number):
+    if x < 0 or y < 0:
+        return False  # Tu ne peux pas aller dans les coordonnées négatives
+
+    number = x*x + 3*x + 2*x*y + y + y*y + favorite_number
+    ones = bin(number).count('1')
+    return ones % 2 == 0
+
 if __name__ == "__main__" : 
-   print("--- Jour 13 : Solution ---") 
-   with open(".\2016\jour13\input.txt","r") as f : 
-       data = f.read().splitlines() 
+    print("--- Jour 13 : Solution ---") 
+    entree = 1364 
+    objectif = (31,39)
+    entre_test = 10
+    objectif_test = (7,4)
+
+    print("Test : pour ce qu'on ous montre dans le sujet , je fait ", bfs_shortest_path(entre_test,objectif_test))
+    print("Partie 1 : Le nombre de pas minimum pour atteindre l'objectif est de ",bfs_shortest_path(entree,objectif))
+    print('Patie 2 : Le nombre de case visite en 50 pas est de ', solve_part2(entree,50))
+
--- 2016/jour14/sol.py
+++ 2016/jour14/sol.py
@@ -1,5 +1,53 @@
 # -*- coding: utf-8 -*- 
-if __name__ == "__main__" : 
-   print("--- Jour 14 : Solution ---") 
-   with open(".\2016\jour14\input.txt","r") as f : 
-       data = f.read().splitlines() 
+from hashlib import md5
+
+
+def stretched_md5(s, times=1):
+    h = md5(s.encode()).hexdigest()
+    for _ in range(times-1):
+        h = md5(h.encode()).hexdigest()
+    return h
+
+def first_repeat(s, n):
+    count = 1
+    for i in range(1, len(s)):
+        if s[i] == s[i-1]:
+            count += 1
+            if count == n:
+                return s[i]
+        else:
+            count = 1
+    return None
+
+def solve(mot, stretch=1):
+    keys = []
+    hashes = []
+    ind = 0
+
+    def get_hash(i):
+        if i >= len(hashes):
+            hashes.append(stretched_md5(mot+str(i), stretch))
+        return hashes[i]
+
+    while len(keys) < 64:
+        h = get_hash(ind)
+        c = first_repeat(h, 3)
+        if c:
+            quint = c*5
+            for j in range(ind+1, ind+1001):
+                if quint in get_hash(j):
+                    keys.append(ind)
+                    break
+        ind += 1
+
+    return keys[63]
+
+if __name__ == "__main__": 
+    print("--- Jour 14 : Solution ---") 
+    data = 'ngcjuoqr'
+    data_test = 'abc'
+    print("Attention l'execution est TRES Longue, environ 5 minutes . ")
+    print("Test :", solve(data_test))   # devrait donner 22728
+    print("Réel :", solve(data))
+    print("Test 2 : ",solve(data_test,2017))
+    print("Réel 2 : ", solve(data,2017))
