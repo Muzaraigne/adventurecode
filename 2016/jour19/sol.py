f3dfd42d433f391b3282b59996c65d00cc55bf0b
Louis GOLLIOT <louis.golliot.etu@univ-lille.fr>
2025-09-11 20:12:12 +0200

    jour 19 de 2016

--- 2016/jour19/sol.py
+++ 2016/jour19/sol.py
@@ -1,5 +1,32 @@
 # -*- coding: utf-8 -*- 
+from collections import deque 
+
+def compute(n):
+    q = deque(range(1, n+1))
+    while len(q) > 1:
+        q.append(q.popleft())  
+        q.popleft()            
+    return q[0]
+
+def compute_part2(n):
+    # trouver la plus grande puissance de 3 <= n
+    power = 1
+    while power * 3 <= n:
+        power *= 3
+    l = n - power
+    if l == 0:
+        return n
+    elif l <= power:
+        return l
+    else:
+        return 2*l - power
+
 if __name__ == "__main__" : 
-   print("--- Jour 19 : Solution ---") 
-   with open(".\2016\jour19\input.txt","r") as f : 
-       data = f.read().splitlines() 
+    print("--- Jour 19 : Solution ---") 
+    data = 3014603
+    data_test = 5
+
+    print(compute(5))
+    print(compute_part2(5))
+    print("Partie 1 : le dernier surviants est ",compute(data))
+    print("Partie 1 : le dernier surviants est ",compute_part2(data))
