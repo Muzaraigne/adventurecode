8850eaef04a06ac5354ad609e200ffcef802ab17
Muzaraigne <test>
2026-04-06 23:06:29 +0200

    remaniment des chemin de fichier

---
+++ Language C/2018/day_9/solution.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../utilities.h"
+
+
+typedef struct List {
+    int value;
+    struct List *next; // Horaire
+    struct List *prev; // Anti-horaire
+} List;
+
+
+int main(void) {
+    int player = 466;
+    int max_marble = 71436;
+
+	// Ton code ici
+
+	free_lines(input);
+	return 0;
+}
