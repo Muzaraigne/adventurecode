d4cddfb3bbbf3d3bef969a336125de568563724f
Muzaraigne <113132027+Muzaraigne@users.noreply.github.com>
2026-05-11 14:11:41 +0200

    Day02 finish

--- JavaAoc/src/main/java/org/example/years/y2019/Day02.java
+++ JavaAoc/src/main/java/org/example/years/y2019/Day02.java
@@ -1,30 +1,63 @@
 package org.example.years.y2019;
 
+import java.util.ArrayList;
+
 import org.example.core.Day;
+
 
 public class Day02 implements Day {
     
     @Override
     public Integer partOne(String input) {
-      List<Integer> op= new ArrayList();
-      for(String c : input.split(",");){
-        op.add(Integer.parseInt(c);
-      }
-      int cursor = 0;
-      while(cursor != 99){
-        if(op[cursor] == 1){
-          op[cursor+3] = op[cursor+1] + op[cursor+2];
-        }
-        else if (op[cursor] == 2){
-          op[cursor+3] = op[cursor+1] * op[cursor+2];
-        }
-        cursor +=4;
-      }
-      return 0;
+      ArrayList<Integer> op = parse(input);
+      op.set(1, 12);
+      op.set(2, 2);
+      cycle(op);
+      return op.get(0);
     }
+
+	private ArrayList<Integer> parse(String input) {
+		ArrayList<Integer> op= new ArrayList<>();
+		  for (String c : input.trim().split(",")) {
+			    op.add(Integer.parseInt(c.trim()));
+			}
+		return op;
+	}
+
+	private void cycle(ArrayList<Integer> op) {
+		int cursor = 0;
+		  while (op.get(cursor) != 99) {
+			    int opcode = op.get(cursor);
+			    int pos1 = op.get(cursor + 1);
+			    int pos2 = op.get(cursor + 2);
+			    int pos3 = op.get(cursor + 3);
+
+			    if (opcode == 1) {
+			        op.set(pos3, op.get(pos1) + op.get(pos2));
+			    } else if (opcode == 2) {
+			        op.set(pos3, op.get(pos1) * op.get(pos2));
+			    } else {
+			        throw new RuntimeException("Opcode inconnu : " + opcode);
+			    }
+
+			    cursor += 4;
+			}
+	}
 
     @Override
     public Integer partTwo(String input) {
+    	int obj = 19690720;
+    	for(int i = 0;i<99;i++) {
+    		for(int j=0;j<99;j++) {
+    			ArrayList<Integer> op = parse(input);
+    			op.set(1, i);
+    			op.set(2, j);
+    			cycle(op);
+    			if(op.get(0) == obj) {
+    				return 100 *i + j;
+    			}
+    		}
+    	}
       return 0;
     }
 
